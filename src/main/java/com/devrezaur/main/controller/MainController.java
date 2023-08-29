ff24bddbbd4e7587dbfda3aba2ef4e6d9cbeb261
Rezaur Rahman <os-rezaur.rahman@rakuten.com>
2023-08-29 08:40:34 +0600

    Initial code added

--- src/main/java/com/devrezaur/main/controller/MainController.java
+++ src/main/java/com/devrezaur/main/controller/MainController.java
@@ -1,2 +1,47 @@
-package com.devrezaur.main.controller;public class MainController {
+package com.devrezaur.main.controller;
+
+import com.devrezaur.main.model.Candidate;
+import com.devrezaur.main.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home-page.html";
+    }
+
+    @PostMapping("/find-by-id")
+    public String findById(@RequestParam Integer id, Model model) {
+        model.addAttribute("candidate", candidateService.findById(id));
+        return "candidate-page.html";
+    }
+
+    @GetMapping("/add-candidate")
+    public String addCandidate() {
+        return "add-candidate-page.html";
+    }
+
+    @PostMapping("/save-candidate")
+    public String saveCandidate(@ModelAttribute Candidate candidate) {
+        candidateService.addCandidate(candidate);
+        return "home-page.html";
+    }
+
+    @GetMapping("/list-all-candidates")
+    public String listAllCandidates(Model model) {
+        model.addAttribute("candidateList", candidateService.getAllCandidate());
+        return "all-candidates-page.html";
+    }
+
 }
